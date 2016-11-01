f17fb5850e3c7bddadc435b1f45ae7d57400b28d
Christian Gaetano <get@christiangaeta.no>
2016-11-01 15:20:58 -0400

    Add chart click handler with debugging Right now the click handler simply performs a hitTest on the chart and logs the returned HitTestInfo. Right now I'm having trouble with getting the HitTestInfo to return the object for the panel clicked, so this is still a WIP

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -2,8 +2,21 @@
 'use strict';
 var DataSourceAdapter = require('./lib/data/DAL/DataSourceAdapter');
 
-DataSourceAdapter.getChartDataSource(function(dataArray) {
-    let mySunburst = new wijmo.chart.hierarchical.Sunburst('#periodic-sunburst');
+// Hook up plain old DOM event handlers
+let domSunburst = document.getElementById('periodic-sunburst');
+if (domSunburst.addEventListener) { // all browsers except IE before version 9
+    domSunburst.addEventListener("click", sunburstClicked, false);
+} else {
+    if (domSunburst.attachEvent) { // IE before version 9
+        domSunburst.attachEvent("click", sunburstClicked);
+    }
+}
+
+// Declare the sunburst object in the global scope so we can reference it in
+// event handlers, etc.
+let mySunburst;
+DataSourceAdapter.getChartDataSource(function (dataArray) {
+    mySunburst = new wijmo.chart.hierarchical.Sunburst('#periodic-sunburst');
     // Let the Sunburst Chart know we're going to start making changes
     mySunburst.beginUpdate();
 
@@ -14,14 +27,23 @@ DataSourceAdapter.getChartDataSource(function(dataArray) {
     mySunburst.dataLabel.position = 'Center';
     mySunburst.dataLabel.content = '{name}';
 
-    console.log(dataArray);
     mySunburst.itemsSource = dataArray;
     mySunburst.binding = 'value';
     mySunburst.bindingName = ['groupName', 'subGroupName', 'eleSymbol'];
     mySunburst.childItemsPath = ['subGroups', 'elements'];
 
     mySunburst.endUpdate();
+
+    console.log('Sunburst chart initialized:');
+    console.log(mySunburst);
 });
+
+function sunburstClicked(event) {
+    let mouseX = event.clientX, mouseY = event.clientY;
+    let hitTestInfo = mySunburst.hitTest(mouseX, mouseY);
+    console.log('Gathered HitTestInfo for click:');
+    console.log(hitTestInfo);
+}
 
 },{"./lib/data/DAL/DataSourceAdapter":2}],2:[function(require,module,exports){
 'use strict';
