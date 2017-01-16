406194aaf9f430fc679b39dd5fcdbc712ccbf362
Christian Gaetano <get@christiangaeta.no>
2017-01-16 12:10:40 -0500

    Refactory PropertiesTile Code for fixed hitTest function
    The hitTest function was broken before, but it now properly returns a JavaScript item representing the clicked chart item. This makes displaying properties for the different panels much easier. (ViewAdapter no longer needed! Happy retirement!)

--- src/view/PropertiesTile.js
+++ src/view/PropertiesTile.js
@@ -6,20 +6,19 @@
  *
  */
 
-// Pull in dependencies
-const Element = require('../data/model/Element');
-const SubGroup = require('../data/model/SubGroup');
-const Group = require('../data/model/Group');
-
 /**
  * Initializes a PropertiesTile Object to handle the DOM functions, events and layouts of an
  * element that will display the properties
  *
  * @param {Object} propertiesTileDomElement - the DOM element to initialize as the properties tile
  */
-const PropertiesTile = function _PropertiesTile(propertiesTileDomElement) {
-  this.domElement = propertiesTileDomElement;
+const PropertiesTile = function _PropertiesTile() {
+  this.propertiesTileDomElement = undefined;
+  this.NEW_LINE_DELIM = '<br>';
+};
 
+PropertiesTile.prototype.attachToDom = function _attachToDom(element) {
+  this.propertiesTileDomElement = element;
   this.centerInParent();
 };
 
@@ -27,7 +26,8 @@ const PropertiesTile = function _PropertiesTile(propertiesTileDomElement) {
  * Centers the properties tile element in its parent container
  */
 PropertiesTile.prototype.centerInParent = function _centerInParent() {
-  const tile = this.domElement;
+  if (!this.propertiesTileDomElement) { return; }
+  const tile = this.propertiesTileDomElement;
   const parentElement = tile.parentElement;
 
   tile.style.top = `${(parentElement.offsetHeight / 2) - (tile.offsetHeight / 2)}px`;
@@ -38,7 +38,10 @@ PropertiesTile.prototype.centerInParent = function _centerInParent() {
  * Shows the properties tile element by changing its visibility style
  */
 PropertiesTile.prototype.show = function _show() {
-  const tile = this.domElement;
+  if (!this.propertiesTileDomElement) {
+    return;
+  }
+  const tile = this.propertiesTileDomElement;
   tile.style.visibility = 'visible';
 };
 
@@ -46,11 +49,12 @@ PropertiesTile.prototype.show = function _show() {
  * Hides the properties tile element by changing its visibility style
  */
 PropertiesTile.prototype.hide = function _hide() {
-  const tile = this.domElement;
+  if (!this.propertiesTileDomElement) {
+    return;
+  }
+  const tile = this.propertiesTileDomElement;
   tile.style.visibility = 'hidden';
 };
-
-const NEW_LINE_DELIM = '<br>';
 
 /**
  * Initializes an info panel for the provided Group, SubGroup or Element object then shows the
@@ -59,6 +63,9 @@ const NEW_LINE_DELIM = '<br>';
  * @param {Object} infoObject a Group, SubGroup or Element object
  */
 PropertiesTile.prototype.showInfoPanel = function _showInfoPanel(infoObject) {
+  if (!this.propertiesTileDomElement) {
+    return;
+  }
   if (typeof (infoObject) === 'undefined') { // this will happen when the user clicks "randomly" on the page - hide the tile
     this.hide();
   } else { // the user selected something on the chart, display the appropriate info
@@ -69,38 +76,38 @@ PropertiesTile.prototype.showInfoPanel = function _showInfoPanel(infoObject) {
     groupInfoPane.style.display = 'none';
     subGroupInfoPane.style.display = 'none';
     elementInfoPane.style.display = 'none';
-    if (infoObject instanceof Group) { // infoObject is a Group
+    if (infoObject.groups && infoObject.groups.length > 0) { // infoObject is a Group
       // Save the subGroup-listing element in a variable since we use it a lot
       const subGroupListing = document.getElementById('subGroup-listing');
-      document.getElementById('group-name').innerText = infoObject.groupName;
+      document.getElementById('group-name').innerText = infoObject.name;
       subGroupListing.innerHTML = '';
       // Show all SubGroups and the number of elements in each
-      for (let i = 0; i < infoObject.subGroups.length; i += 1) {
-        subGroupListing.innerHTML += infoObject.subGroups[i].subGroupName;
+      for (let i = 0; i < infoObject.groups.length; i += 1) {
+        subGroupListing.innerHTML += infoObject.groups[i].name;
         subGroupListing.innerHTML += ' ';
-        subGroupListing.innerHTML += `(${infoObject.subGroups[i].elements.length})`;
-        subGroupListing.innerHTML += NEW_LINE_DELIM;
+        subGroupListing.innerHTML += `(${infoObject.groups[i].items.length})`;
+        subGroupListing.innerHTML += this.NEW_LINE_DELIM;
       }
       groupInfoPane.style.display = 'block'; // show the group info pane
-    } else if (infoObject instanceof SubGroup) {
+    } else if (infoObject.items) {
       // infoObject is a SubGroup so show the number of elements and properties
-      document.getElementById('subGroup-name').innerText = infoObject.subGroupName;
-      document.getElementById('num-elements').innerText = infoObject.elements.length;
+      document.getElementById('subGroup-name').innerText = infoObject.name;
+      document.getElementById('num-elements').innerText = infoObject.items.length;
       // Save the characteristic-listing element in a variable since we use it a lot
       const characteristicListing = document.getElementById('characteristic-listing');
       characteristicListing.innerHTML = '';
       // Split the characteristics up so we can display them as a list
-      const characteristicsArray = infoObject.characteristics.split(',');
+      const characteristicsArray = infoObject.elementProperties.split(',');
       for (let i = 0; i < characteristicsArray.length; i += 1) {
         characteristicListing.innerHTML += characteristicsArray[i];
-        characteristicListing.innerHTML += NEW_LINE_DELIM;
+        characteristicListing.innerHTML += this.NEW_LINE_DELIM;
       }
       subGroupInfoPane.style.display = 'block'; // show the subGroup info pane
-    } else if (infoObject instanceof Element) { // the infoObject is an Element, display its props
-      document.getElementById('element-symbol').innerText = infoObject.eleSymbol;
-      document.getElementById('element-name').innerText = infoObject.elementName;
-      document.getElementById('atomic-number').innerText = infoObject.atomicNumber;
-      document.getElementById('atomic-weight').innerText = Number(infoObject.atomicWeight).toFixed(2);
+    } else { // the infoObject is an Element, display its props
+      document.getElementById('element-symbol').innerText = infoObject.symbol;
+      document.getElementById('element-name').innerText = infoObject.element;
+      document.getElementById('atomic-number').innerText = infoObject['atomic-number'];
+      document.getElementById('atomic-weight').innerText = Number(infoObject['atomic-weight']).toFixed(2);
       elementInfoPane.style.display = 'block'; // show element info pane
     }
 
